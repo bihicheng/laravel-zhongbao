8a1aa89c4d2d3178dc823db520d02c3179de4de8
huwence <huwence@mugeda.com>
2016-05-24 17:47:32 +0800

    first commit

---
+++ database/migrations/2016_05_23_014847_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->primary('id');
+            $table->string('name', 128);
+            $table->index('name');
+            $table->text('description');
+            $table->dateTime('create_at');
+            $table->dateTime('update_at');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
